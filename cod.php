289b043138cd6ea50031a079e5fc5949c36746f2
Lavínia <lavsdnts@gmail.com>
2025-02-12 11:08:36 -0300

    Add files via upload

---
+++ cod.php
@@ -0,0 +1,153 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $id = (isset($_POST["id"]) && $_POST["id"] != null) ? $_POST["id"] : "";
+    $nome = (isset($_POST["nome"]) && $_POST["nome"] != null) ? $_POST["nome"] : "";
+    $senha = (isset($_POST["senha"]) && $_POST["senha"] != null) ? $_POST["senha"] : "";
+} else if (!isset($id)) {
+    // Se não se não foi setado nenhum valor para variável $id
+    $id = (isset($_GET["id"]) && $_GET["id"] != null) ? $_GET["id"] : "";
+    $nome = NULL;
+    $senha = NULL;
+}
+ 
+// Cria a conexão com o banco de dados
+try {
+    $conexao = new PDO('pgsql:host=127.0.0.1;port=5432;dbname=20221214010012',
+	'postgres', 'pabd');
+    $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conexao->exec("SET client_encoding TO 'UTF8'");
+} catch (PDOException $erro) {
+    echo "Erro na conexão:".$erro->getMessage();
+}
+ 
+// Bloco If que Salva os dados no Banco - atua como Create e Update
+if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "save" && $nome != "") {
+    try {
+        if ($id != "") {
+            $stmt = $conexao->prepare("UPDATE funcionario SET nome=?, senha=? WHERE id = ?");
+            $stmt->bindParam(3, $id);
+        } else {
+            $stmt = $conexao->prepare("INSERT INTO funcionario (nome, senha) VALUES (?, ?)");
+        }
+        $stmt->bindParam(1, $nome);
+        $stmt->bindParam(2, $senha);
+ 
+        if ($stmt->execute()) {
+            if ($stmt->rowCount() > 0) {
+                echo "Dados cadastrados com sucesso!";
+                $id = null;
+                $nome = null;
+                $senha = null;
+            } else {
+                echo "Erro ao tentar efetivar cadastro";
+            }
+        } else {
+            throw new PDOException("Erro: Não foi possível executar a declaração sql");
+        }
+    } catch (PDOException $erro) {
+        echo "Erro: ".$erro->getMessage();
+    }
+}
+ 
+// Bloco if que recupera as informações no formulário, etapa utilizada pelo Update
+if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "upd" && $id != "") {
+    try {
+        $stmt = $conexao->prepare("SELECT * FROM funcionario WHERE id = ?");
+        $stmt->bindParam(1, $id, PDO::PARAM_INT);
+        if ($stmt->execute()) {
+            $rs = $stmt->fetch(PDO::FETCH_OBJ);
+            $id = $rs->id;
+            $nome = $rs->nome;
+            $senha = $rs->senha;
+        } else {
+            throw new PDOException("Erro: Não foi possível executar a declaração sql");
+        }
+    } catch (PDOException $erro) {
+        echo "Erro: ".$erro->getMessage();
+    }
+}
+ 
+// Bloco if utilizado pela etapa Delete
+if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "del" && $id != "") {
+    try {
+        $stmt = $conexao->prepare("DELETE FROM funcionario WHERE id = ?");
+        $stmt->bindParam(1, $id, PDO::PARAM_INT);
+        if ($stmt->execute()) {
+            echo "Registo foi excluído com êxito";
+            $id = null;
+        } else {
+            throw new PDOException("Erro: Não foi possível executar a declaração sql");
+        }
+    } catch (PDOException $erro) {
+        echo "Erro: ".$erro->getMessage();
+    }
+}
+?>
+<!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="UTF-8">
+            <title>Restaurante D'ullà</title>
+        </head>
+        <body>
+            <form action="?act=save" method="POST" name="form1" >
+                <h1>Gerenciamento de funcionários</h1>
+                <hr>
+                <input type="hidden" name="id" <?php
+                 
+                // Preenche o id no campo id com um valor "value"
+                if (isset($id) && $id != null || $id != "") {
+                    echo "value=\"{$id}\"";
+                }
+                ?> />
+                Nome:
+               <input type="text" name="nome" <?php
+ 
+               // Preenche o nome no campo nome com um valor "value"
+               if (isset($nome) && $nome != null || $nome != "") {
+                   echo "value=\"{$nome}\"";
+               }
+               ?> />
+               Senha:
+               <input type="text" name="senha" <?php
+ 
+               // Preenche a senha no campo senha com um valor "value"
+               if (isset($senha) && $senha != null || $senha != "") {
+                   echo "value=\"{$senha}\"";
+               }
+               ?> />
+            
+               <input type="submit" value="salvar" />
+               <input type="reset" value="Novo" />
+               <hr>
+            </form>
+            <table border="1" width="100%">
+                <tr>
+                    <th>Nome</th>
+                    <th>Senha</th>
+                    <th>Ações</th>
+                </tr>
+                <?php
+ 
+                // Bloco que realiza o papel do Read - recupera os dados e apresenta na tela
+                try {
+                    $stmt = $conexao->prepare("SELECT * FROM funcionario");
+                    if ($stmt->execute()) {
+                        while ($rs = $stmt->fetch(PDO::FETCH_OBJ)) {
+                            echo "<tr>";
+                            echo "<td>".$rs->nome."</td><td>".$rs->senha."</td><td><center>"
+                                       ."<center><a href=\"?act=upd&id=".$rs->id."\">[Alterar]</a>"
+                                       ."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+                                       ."<a href=\"?act=del&id=".$rs->id."\">[Excluir]</a></center></td>";
+                            echo "</tr>";
+                        }
+                    } else {
+                        echo "Erro: Não foi possível recuperar os dados do banco de dados";
+                    }
+                } catch (PDOException $erro) {
+                    echo "Erro: ".$erro->getMessage();
+                }
+                ?>
+            </table>
+        </body>
+    </html>
